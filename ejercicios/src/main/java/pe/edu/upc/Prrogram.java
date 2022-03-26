3de92b60c13f76698e1ae62b7de0ac06e7f4fd7f
Maria123-sys <mm9744280@gmail.com>
2022-03-26 11:05:03 -0500

    Creacion de una clase y objeto student

---
+++ ejercicios/src/main/java/pe/edu/upc/Prrogram.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+public class Prrogram {
+
+	public static void main(String[] args) {
+		
+		Student student=new Student();
+		student.setFirstName("Maria Isabel Moreno Lozano");
+		System.out.println(student.getFirstName());
+
+	}
+
+}
